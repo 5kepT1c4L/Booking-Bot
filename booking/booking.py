7a9bd526cb0cab6adf3b6597be7fa4e33ed37870
Eric Jing <70067413+5kepT1c4L@users.noreply.github.com>
2021-09-17 23:57:14 -0400

    Program now clicks on website's currency page

--- booking/booking.py
+++ booking/booking.py
@@ -1,8 +1,27 @@
 from selenium import webdriver
-
+import booking.constants as const
+import os
 
 class Booking(webdriver.Chrome):
 
-    def __init__(self, driver_path=r"C:\chromedriver_win32"):
+    def __init__(self, driver_path= os.pathsep + "C:\chromedriver_win32", teardown=False):
+        self.teardown = teardown
         self.driver_path = driver_path
+        os.environ['PATH'] += self.driver_path
         super(Booking, self).__init__()
+        self.implicitly_wait(15)
+        self.maximize_window
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.teardown:
+            self.quit()
+    
+    def land_first_page(self):
+        self.get(const.BASE_URL)
+
+    def change_currency(self, currency=None):
+        currency_element = self.find_element_by_css_selector(
+            'button[data-tooltip-text="Choose your currency"]'
+        )
+        currency_element.click()
+        
--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from booking.booking import Booking
 
-inst = Booking()
-inst.land_first_page()
+with Booking() as bot:
+
+    bot.land_first_page()
+
+    bot.change_currency()
